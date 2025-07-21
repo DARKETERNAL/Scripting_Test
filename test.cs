f634df67f44d98f438945edd88d27a3c29a0d876
DARKETERNAL <tomailsandstuff@gmail.com>
2025-07-21 07:12:44 -0500

    Create test.cs

---
+++ test.cs
@@ -0,0 +1,5 @@
+//Escriba una función que reciba una cantidad entera de segundos y retorne un string que muestre esa cantidad en formato HH:MM:SS. NO utilizar la clase DateTime.
+public string FormatTime(int secs)
+{
+  return string.Empty()
+}
